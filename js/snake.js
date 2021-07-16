56f0cd0573aab8fc921e84bf141b6ff6fb867699
c4f3m1 <cafemi.on@gmail.com>
2021-07-16 13:15:32 +0200

    Implement basic canvas layout

---
+++ js/snake.js
@@ -0,0 +1,18 @@
+class snake {
+
+    constructor(size, vel){
+        this.size = size;
+        this.vel = vel;
+    }
+
+    get size(){
+        return this.size;
+    }
+    get vel(){
+        return this.vel;
+    }
+
+
+
+
+}
